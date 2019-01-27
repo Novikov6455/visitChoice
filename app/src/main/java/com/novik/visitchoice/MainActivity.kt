698d7cd155af4c1eb2c6ce2a2bd850d3e634805e
Novikov6455 <novikov6455@gmail.com>
2019-01-26 22:23:18 -0800

    Initial commit

---
+++ app/src/main/java/com/novik/visitchoice/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.novik.visitchoice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val visitList = arrayListOf("Tahoe lake", "Carmel", "Yosemity", "Las Vegas", "Kolomna")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = java.util.Random()
+            val randomVisit = random.nextInt(visitList.count())
+            selectedVisitTxt.text = visitList[randomVisit]
+        }
+
+        addVisitBtn.setOnClickListener {
+            val newVisit = addVisitTxt.text.toString().trimStart()
+            if (newVisit != "") visitList.add(newVisit)
+            addVisitTxt.text?.clear()
+            println(visitList)
+        }
+
+    }
+}
